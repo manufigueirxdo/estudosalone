94d541246cedea403b4ee26462db8d2dd97fd642
Manuelly Figueiredo <camilenunes650@gmail.com>
2026-03-02 20:05:02 -0300

    Delete 02_condicionas_ifelse directory

--- 02_condicionas_ifelse/calculadora_salario.py
+++
@@ -1,8 +0,0 @@
-nome_do_funcionario = (input("Nome do funcionário?"))
-salario_bruto = float(input("Qual o salário bruto dele(a)?"))
-
-desconto = salario_bruto * 0.11
-salario_liquido = salario_bruto - desconto
-
-e_maior = salario_liquido > 2000.00
-print(f"O salário líquido é maior que R$2000.00? {e_maior}")
